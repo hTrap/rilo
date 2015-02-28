6c130e62b3f30e0b352ed3837f5870705f723f98
Parth Oberoi <htrapdev@gmail.com>
2015-02-28 23:21:12 +0530

    Create a new rails project

---
+++ bitsofsoup/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bitsofsoup::Application.config.secret_key_base = 'aaa6f1327150afa896aefa47ceff78dfb975e69c417d6b9358ab9b055634889a6925179c77b0183407decba60f981efeb4fe23a06fed9af1305c098a19d19bbe'
---
+++ bitsofsoup/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bitsofsoup::Application.config.session_store :cookie_store, key: '_bitsofsoup_session'
