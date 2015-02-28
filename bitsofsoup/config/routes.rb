14bf9e32202b374e97b0c2af949b89598e24ca31
Parth Oberoi <htrapdev@gmail.com>
2015-03-01 01:15:53 +0530

    Adding routes

--- bitsofsoup/config/routes.rb
+++ bitsofsoup/config/routes.rb
@@ -1,4 +1,5 @@
 Bitsofsoup::Application.routes.draw do
+  resources :categories, :soups
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
