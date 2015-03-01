162b95942713e5cfe8f666ace6f995b88651865a
htrap <htrapdev@gmail.com>
2015-03-01 23:32:43 +0530

    Add json rendering for soups

--- bitsofsoup/app/controllers/soups_controller.rb
+++ bitsofsoup/app/controllers/soups_controller.rb
@@ -1,5 +1,9 @@
 class SoupsController < ApplicationController
 	def show
 		@soup = Soup.find(params[:id])
+		respond_to do |format|
+			format.html
+			format.json  { render json: @soup }
+		end
 	end
 end
